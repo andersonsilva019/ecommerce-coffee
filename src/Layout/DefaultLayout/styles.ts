2496aa3ec818b30339b7b4437806fab2844ed399
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-07-24 21:26:17 -0300

    Creating DefaultLayout component

---
+++ src/Layout/DefaultLayout/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const DefaultLayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  max-width: 74rem;
+
+  margin: 0 auto;
+  padding: 0 2rem;
+`
