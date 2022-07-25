6773664592b1e106e5392ae14ecb1d9f9b1d9dc3
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-07-25 09:03:51 -0300

    Adding fonts-size to defaultTheme

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -16,5 +16,13 @@ export const defaultTheme = {
     'gray-300': '#F3F2F2',
     white: '#FFFFFF',
     background: '#FAFAFA',
+  },
+  fonts: {
+    '10px': '0.625rem',
+    '12px': '0.75rem',
+    '14px': '0.875rem',
+    '16px': '1rem',
+    '20px': '1.25rem',
+    '24px': '1.5rem',
   }
-}
+} as const
